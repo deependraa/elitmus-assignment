4f0195749cb74b4a6e7fb8eaff4e17cc8b6cc128
Deependra singh <deependrasingh158@gmail.com>
2022-01-03 14:02:42 +0530

    l

--- assignment/settings.py
+++ assignment/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-oos3=ymyaker(f_&$jk+8c84i$bvn#upv!=ogi0!)=e4sc#ay6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['elitmusdeependra.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = []
 
 
 # Application definition

08ff0a1616dcca1a3dc323c5cd115dfbb49183f0
Deependra singh <deependrasingh158@gmail.com>
2022-01-03 14:05:37 +0530

    asd

--- assignment/settings.py
+++ assignment/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-oos3=ymyaker(f_&$jk+8c84i$bvn#upv!=ogi0!)=e4sc#ay6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [ 'elitmusdeependra.herokuapp.com' ]
+ALLOWED_HOSTS = [ 'elitmusdeependra.herokuapp.com' , '127.0.0.1']
 
 
 # Application definition
